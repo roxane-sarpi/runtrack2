6cd0261e2aaf8011c5d2cf18e1799309cfad17e2
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-03 16:51:32 +0200

    ajout de l'algorithme PHP

--- Jour4/job2/index.php
+++ Jour4/job2/index.php
@@ -13,6 +13,23 @@
     <label>Âge : <input type="text" name="age"></label><br>
     <input type="submit" value="Envoyer">
 </form>
+<?php
 
+if (!empty($_GET)) {
+    echo "<h2>Résultats :</h2>";
+    echo "<table border='1' cellpadding='5'>";
+    echo "<tr><th>Argument</th><th>Valeur</th></tr>";
+
+  
+    foreach ($_GET as $argument => $valeur) {
+        echo "<tr>";
+        echo "<td>$argument</td>";
+        echo "<td>$valeur</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
+?>
 </body>
 </html>
