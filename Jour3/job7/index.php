e2c3da107856b94d4905ab1eff92c5117a03fe69
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-03 13:22:30 +0200

    calcul sans fonction système

--- Jour3/job7/index.php
+++ Jour3/job7/index.php
@@ -1,17 +1,24 @@
 <?php
+
 $str = "Certaines choses changent, et d'autres ne changeront jamais.";
 
 
-$n = strlen($str);
+$n = 0;
+while (isset($str[$n])) {
+    $n++;
+}
 
 
 $newStr = "";
 
+
 for ($i = 1; $i < $n; $i++) {
-    $newStr .= $str[$i];   
+    $newStr .= $str[$i];
 }
 
+
 $newStr .= $str[0];
+
 
 echo $newStr;
 ?>
