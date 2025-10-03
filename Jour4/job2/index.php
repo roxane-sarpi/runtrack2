16b1990a43a8be9e36fb9b580d260878d21ad699
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-03 16:46:31 +0200

    formulaire HTML

---
+++ Jour4/job2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription au marathon</title>
+</head>
+<body>
+
+
+<form method="get" action="">
+    <label>Prénom : <input type="text" name="prenom"></label><br>
+    <label>Nom : <input type="text" name="nom"></label><br>
+    <label>Âge : <input type="text" name="age"></label><br>
+    <input type="submit" value="Envoyer">
+</form>
+
+</body>
+</html>
