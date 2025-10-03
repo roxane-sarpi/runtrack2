efdf9b57874966e50ca75081ebc9515a19755e19
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-03 14:24:02 +0200

    formulaire HTLM GET

---
+++ Jour4/job1/index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire GET</title>
+</head>
+<body>
+
+    <h2>Formulaire GET</h2>
+    <form method="get" action="">
+        <label>Nom : </label>
+        <input type="text" name="nom"><br><br>
+
+        <label>Prénom : </label>
+        <input type="text" name="prenom"><br><br>
+
+        <label>Âge : </label>
+        <input type="text" name="age"><br><br>
+
+        <input type="submit" value="Envoyer">
+    </form>
+
+</body>
+</html>
