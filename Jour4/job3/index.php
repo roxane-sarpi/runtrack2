b58726371655a5181c0829fc361ed0f2b443d613
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-06 09:58:14 +0200

    formulaire HTML POST

--- Jour4/job3/index.php
+++ Jour4/job3/index.php
@@ -7,3 +7,20 @@ foreach ($posts as $post) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Nombre POST</title>
+</head>
+<body>
+    <h1>Formulaire POST</h1>
+ <form method="post">
+    <label>Prénom : <input type="text" name="prenom"></label><br>
+    <label>Pseudo : <input type="text" name="pseudo"></label><br>
+    <label>Date de naissance : <input type="text" name="date_naissance"></label><br>
+    <input type="submit" name="envoyer" value="Envoyer">
+  </form>
+</body>
+</html>
