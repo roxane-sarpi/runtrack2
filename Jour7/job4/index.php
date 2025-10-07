b4b4290618a0354d0e3d2956b20266676a8589df
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-07 09:40:19 +0200

    fonction calcul complète

--- Jour7/job4/index.php
+++ Jour7/job4/index.php
@@ -1,3 +1,24 @@
 <?php
-function ($a, $operator, $b)
+function ($a, $operator, $b) {
+    if ($operator === '+') {
+        return $a + $b;
+    } elseif ($operator === '-') {
+        return $a - $b;
+    } elseif ($operator === '*') {
+        return $a * $b; 
+    } elseif ($operator === '/') {
+        if ($b != 0) {
+            return $a / $b;
+        } else {
+            return "Erreur : Division par zéro impossible.";
+        }
+    } elseif ($operator === '%') {
+        if ($b != 0) {
+            return $a % $b;
+        } else {
+            return "Erreur : Modulo par zéro impossible.";
+        }
+    }
+}
+
 ?>
