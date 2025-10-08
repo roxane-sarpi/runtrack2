c7331fa500d2a2f449e8af75b713d2742293c7e3
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-08 14:16:04 +0200

    ajout html cookies

--- Jour8/job1/index.php
+++ Jour8/job1/index.php
@@ -9,7 +9,23 @@ if (isset($_POST['reset'])) {
     if (!isset($_SESSION['nbvisites'])) {
         $_SESSION['nbvisites'] = 0;
     }
-    
+
     $_SESSION['nbvisites']++;
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Compteur de visites</title>
+</head>
+<body>
+    <h1>Compteur de visites</h1>
+    <p>Nombre de visites : <strong><?= $_SESSION['nbvisites']; ?></strong></p>
+
+    <form method="post">
+        <button type="submit" name="reset">Reset</button>
+    </form>
+</body>
+</html>
