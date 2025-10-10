3e9904b42d8e6b1c887888bf6cb63b718ab4b6e8
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-10 11:00:03 +0200

    correction du renvoi du formulaire à chaque rafraichissement de page

--- Jour8/job2/index.php
+++ Jour8/job2/index.php
@@ -1,16 +1,31 @@
 <?php
-if(isset($_COOKIE['nbvisites'])) {
+
+if (isset($_POST['reset'])) {
+    setcookie('nbvisites', '', time() - 3600);
+    
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+if (isset($_COOKIE['nbvisites'])) {
     $nbvisites = $_COOKIE['nbvisites'] + 1;
 } else {
     $nbvisites = 1;
 }
 
-setcookie('nbvisites', $nbvisites, time() + 3600);
-
-echo "Nombre de visites : " . $nbvisites;
-
-if (isset($_POST['reset'])) {
-    setcookie('nbvisites', '', time() - 3600);
-}
-
+setcookie('nbvisites', $nbvisites, time() + 365*24*3600);
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Compteur de visites PHP</title>
+</head>
+<body>
+  <p>Nombre de visites : <?= $nbvisites ?></p>
+
+  <form method="post">
+    <button type="submit" name="reset">Reset</button>
+  </form>
+</body>
+</html>
