dc923dde52d461b398c8d1ef01d26a2dfdb1371d
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-10 16:27:30 +0200

    connexion

--- Jour8/job4/index.php
+++ Jour8/job4/index.php
@@ -4,5 +4,12 @@ if (isset($_POST['deco])) {
 setcookie ('prenom', '', time() - 3600);
 header ("Location: " . $_SERVER['PHP_SELF']);
 exit;
-}']))
+}']
+
+if (isset($_POST['connexion']) && !empty($_POST['prenom'])) {
+    $prenom = htmlspecialchars($_POST['prenom']);
+    setcookie('prenom', $prenom, $expire); 
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
 ?>
