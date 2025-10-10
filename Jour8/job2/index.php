d5739c9a493386283cd4ed63a136c6d621f7e9af
Roxane Sarpi <roxane.sarpi@laplateforme.io>
2025-10-10 10:24:37 +0200

    suppression du cookie

--- Jour8/job2/index.php
+++ Jour8/job2/index.php
@@ -4,5 +4,13 @@ if(isset($_COOKIE['nbvisites'])) {
 } else {
     $nbvisites = 1;
 }
+
 setcookie('nbvisites', $nbvisites, time() + 3600);
+
+echo "Nombre de visites : " . $nbvisites;
+
+if (isset($_POST['reset'])) {
+    setcookie('nbvisites', '', time() - 3600);
+}
+
 ?>
